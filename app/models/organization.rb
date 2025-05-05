2586db5a852edebd62ee6f14ee23f93bf14e75f0
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-05-05 11:57:08 -0700

    fix bug

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,10 +1,8 @@
 class Organization < ApplicationRecord
   has_many :users
-  accepts_nested_attributes_for :users
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  acts_as_tenant(:organization)
 end

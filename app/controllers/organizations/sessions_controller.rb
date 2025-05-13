9354b25536492e0d9976d26128d70ba13c2c3f8d
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-05-13 16:46:05 -0700

    fix logout redirect

--- app/controllers/organizations/sessions_controller.rb
+++ app/controllers/organizations/sessions_controller.rb
@@ -7,4 +7,11 @@ class Organizations::SessionsController < Devise::SessionsController
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
   end
+
+  def destroy
+    signed_out = sign_out(resource_name)
+    set_flash_message! :notice, :signed_out if signed_out
+
+    redirect_to root_url(subdomain: nil, host: "localhost", protocol: "http"), allow_other_host: true
+  end
 end
